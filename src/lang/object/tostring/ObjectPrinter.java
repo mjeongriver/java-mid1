4b939b8fcd4ccff238b0a82de4c644061b034ba8
mjeongriver <jennygh2@hanmail.net>
2024-04-01 16:17:38 +0900

    Object와 OCP

--- src/lang/object/tostring/ObjectPrinter.java
+++ src/lang/object/tostring/ObjectPrinter.java
@@ -4,6 +4,7 @@ public class ObjectPrinter {
 
     public static void print(Object object) {
         String string = "객체 정보 출력: " + object.toString();
+        //자바 언어가 기본으로 제공하는 다양한 메서드들은 개발자가 필요에 따라 오버라이딩 해서 사용할 수 있도록 설계되어 있다.
         System.out.println(string);
     }
 }
