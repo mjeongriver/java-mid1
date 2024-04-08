3e77c4c85cb81128bd2d01f168a4371db17718ea
mjeongriver <jennygh2@hanmail.net>
2024-04-08 11:15:41 +0900

    기본형과 참조형의 공유

---
+++ src/lang/immutable/address/RefMain1_1.java
@@ -0,0 +1,19 @@
+package lang.immutable.address;
+
+public class RefMain1_1 {
+
+    public static void main(String[] args) {
+        //참조형 변수는 하나의 인스턴스를 공유할 수 있다.
+
+        Address a = new Address("서울");
+        Address b = a; //a에 있는 참조값 x001을 복사해서 b에 전달한다.
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        b.setValue("부산");
+        System.out.println("부산 -> b");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
