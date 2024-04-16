2f909d26662ff4dfb0065ece822a3f43d55136a8
mjeongriver <jennygh2@hanmail.net>
2024-04-16 12:47:03 +0900

    String 클래스 - 비교

--- src/lang/string/equals/StringEqualsMain2.java
+++ src/lang/string/equals/StringEqualsMain2.java
@@ -6,7 +6,7 @@ public class StringEqualsMain2 {
         String str1 = new String("hello");
         String str2 = new String("hello");
         System.out.println("메서드 호출 비교1: " + isSame(str1, str2));
-        
+
         String str3 = "hello";
         String str4 = "hello";
         System.out.println("메서드 호출 비교2: " + isSame(str3, str4));
