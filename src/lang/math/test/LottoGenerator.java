49679730ca5d974a3cabc3630458e2ab6c73196f
mjeongriver <jennygh2@hanmail.net>
2024-04-25 13:09:17 +0900

    문제와 풀이 2

---
+++ src/lang/math/test/LottoGenerator.java
@@ -0,0 +1,33 @@
+package lang.math.test;
+
+import java.util.Random;
+
+public class LottoGenerator {
+    private final Random random = new Random();
+    private int[] lottoNumbers;
+    private int count;
+
+    public int[] generator() {
+        lottoNumbers = new int[6];
+        count = 0;
+        while(count < 6) {
+            int number = random.nextInt(45) + 1;
+            if(!isUnique(number)) {
+                continue;
+            }
+            lottoNumbers[count] = number;
+            count++;
+        }
+        return lottoNumbers;
+    }
+
+    // 중복 검사
+    public boolean isUnique(int number) {
+        for(int i = 0; i < count; i++) {
+            if(lottoNumbers[i] == number) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
