f49c78222364b748e7b26d3f80c2b9c1ee1c48eb
mjeongriver <jennygh2@hanmail.net>
2024-04-30 13:00:48 +0900

    StringBuilder - 가변 String

---
+++ src/lang/string/builder/StringBuilderMain1_1.java
@@ -0,0 +1,29 @@
+package lang.string.builder;
+
+public class StringBuilderMain1_1 {
+
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("A");
+        sb.append("B");
+        sb.append("C");
+        sb.append("D");
+        System.out.println("sb = " + sb);
+
+        sb.insert(4, "JAVA");
+        System.out.println("insert = " + sb);
+
+        sb.delete(4, 8);
+        System.out.println("delete = " + sb);
+
+        sb.reverse();
+        System.out.println("reverse = " + sb);
+
+        //StringBuilder(가변) -> String(불변)
+        String string = sb.toString();
+        System.out.println("string = " + string);
+
+    }
+
+}
