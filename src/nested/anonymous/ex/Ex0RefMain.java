c3a4e3f058775843f3ec318f8fab5370db399079
mjeongriver <jennygh2@hanmail.net>
2024-05-23 23:19:10 +0900

    익명 클래스 활용 1

---
+++ src/nested/anonymous/ex/Ex0RefMain.java
@@ -0,0 +1,15 @@
+package nested.anonymous.ex;
+
+public class Ex0RefMain {
+
+    public static void hello(String message) {
+        System.out.println("프로그램 시작"); //변하지 않는 부분
+        System.out.println("Hello " + message); //변하는 부분
+        System.out.println("프로그램 종료"); //변하지 않는 부분
+    }
+
+    public static void main(String[] args) {
+        hello("Java");
+        hello("Spring");
+    }
+}
