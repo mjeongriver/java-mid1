7f333ba4351a70ebc5f0aa86432213bc4684f54b
mjeongriver <jennygh2@hanmail.net>
2024-05-26 13:18:23 +0900

    예외 처리가 필요한 이유 1 - 시작

---
+++ src/exception/ex0/MainV0.java
@@ -0,0 +1,23 @@
+package exception.ex0;
+
+import java.util.Scanner;
+
+public class MainV0 {
+
+    public static void main(String[] args) {
+        NetworkServiceV0 service = new NetworkServiceV0();
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("전송할 데이터를 입력하세요: ");
+            String data = scanner.nextLine();
+            if (data.equals("quit")) {
+                break;
+            }
+
+            service.sendMessage(data);
+            System.out.println();
+        }
+        System.out.println("프로그램 종료");
+    }
+}
