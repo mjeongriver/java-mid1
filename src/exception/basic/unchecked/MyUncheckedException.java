29cef597299ecf62f010c1db1dfa89643fc18fc4
mjeongriver <jennygh2@hanmail.net>
2024-05-29 10:00:05 +0900

    자바 예외 처리 4 - 언체크 예외

---
+++ src/exception/basic/unchecked/MyUncheckedException.java
@@ -0,0 +1,12 @@
+package exception.basic.unchecked;
+
+import exception.basic.checked.MyCheckedException;
+
+/**
+ * RuntimeException을 상속받은 예외는 언체크 예외가 된다.
+ */
+public class MyUncheckedException extends RuntimeException {
+    public MyUncheckedException(String message) {
+        super(message);
+    }
+}
